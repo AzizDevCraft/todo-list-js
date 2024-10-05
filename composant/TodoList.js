5d7420094dd41c82417d1f8a74b3166838f47cdd
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-10-05 16:56:35 +0200

    j'ai finaliser le projet

--- composant/TodoList.js
+++ composant/TodoList.js
@@ -8,6 +8,7 @@ import { CreatElement } from "../fonction/DOM.js"
  */
 export class TodoList {
     #todos = []
+    #listTodos = []
 
     constructor (todos) {
         this.#todos = todos 
@@ -16,10 +17,10 @@ export class TodoList {
     appendTo (element) {
         const tpl = document.getElementById ("tpl-todolist").content.cloneNode(true)
         element.append (tpl)
-        const list = document.querySelector (".list-group")
+        this.#listTodos = document.querySelector (".list-group")
         for (let i of this.#todos) {
             const unaTodo = new ListItem (i)
-            unaTodo.appendToo (list)
+            unaTodo.appendToo (this.#listTodos)
         }
         const formInput = document.querySelector ("form")
         formInput.addEventListener ("submit", e => {
@@ -34,9 +35,33 @@ export class TodoList {
                 id : Date.now ()
             }
             const unaTodo = new ListItem (ObjetTodo)
-            unaTodo.prependToo (list)
+            unaTodo.prependToo (this.#listTodos)
             e.currentTarget.reset()
         })
+
+        element.querySelectorAll (".btn-group button").forEach(button => {
+            button.addEventListener ("click", e => this.#toggleFilter (e) )
+        })
+    }
+
+    /**
+    * @param {PointerEvent} e
+    */
+    #toggleFilter (e) {
+        e.preventDefault ()
+        const filter = e.currentTarget.getAttribute ("data-filter")
+        e.currentTarget.parentElement.querySelector(".active").classList.remove ("active")
+        e.currentTarget.classList.add ("active")
+        if (filter === "todo") {
+            this.#listTodos.classList.add ("hide-completed")
+            this.#listTodos.classList.remove ("hide-todo")
+        }else if (filter === "done") {
+            this.#listTodos.classList.add ("hide-todo")
+            this.#listTodos.classList.remove ("hide-completed")
+        }else {
+            this.#listTodos.classList.remove ("hide-todo")
+            this.#listTodos.classList.remove ("hide-completed")
+        }
     }
 }
 
@@ -46,6 +71,7 @@ class ListItem {
 
     constructor (todo) {
         const li = CreatElement ("li", {class: "todo list-group-item d-flex align-items-center"})
+        this.#element = li
         const checkbox = CreatElement ("input", {class: "form-check-input", id: `todo-${todo.id}`, type: "checkbox", checked: todo.completed ? '': null})
         const checkboxLabel = CreatElement ("label", {class: "ms-2 form-check-label", for: `todo-${todo.id}`})
         checkboxLabel.innerText = todo.title
@@ -53,11 +79,13 @@ class ListItem {
         const icon = CreatElement ("i", {class:"bi-trash"})
         button. append (icon)
         li.append (checkbox, checkboxLabel, button)
-        this.#element = li
+        this.toggle (checkbox)
 
         button.addEventListener ("click", e => {
             this.#element.remove ()
         })
+
+        checkbox.addEventListener ("change", e => this.toggle (e.currentTarget))
     }
 
     appendToo (element) {
@@ -66,5 +94,13 @@ class ListItem {
 
     prependToo (element) {
         element.prepend (this.#element)
+    }
+
+    toggle (checkbox) {
+        if (checkbox.checked) {
+            this.#element.classList.add ("is-completed")
+        }else {
+            this.#element.classList.remove ("is-completed")
+        }
     }
 }
