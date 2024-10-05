797c7711747b8efe9289020d02850964517e1cba
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-10-05 16:55:44 +0200

    j'ai corriger un erreur (c lors de l'affichage dans le console)

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ try {
     const data = await getData ("https://jsonplaceholder.typicode.com/todos?_limit=5")
     const list = new TodoList (data)
     list.appendTo (document.getElementById ("todolist"))
-    
+
 } catch (erreur){
     const alertElement = CreatElement ("div", {class : "alert alert-danger m-2", role : "alert"})
     alertElement.innerHTML = `${erreur.name} : ${erreur.message}`
     document.body.prepend (alertElement)
+    console.erreur (erreur)
 }
 
